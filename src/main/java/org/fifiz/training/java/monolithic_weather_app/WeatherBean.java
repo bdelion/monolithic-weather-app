17a627232bfb79b5183ee5b185d43bf3c00d3dd2
Bertrand DELION <bertrand.delion@free.fr>
2019-03-02 16:15:29 +0100

    First try KO

--- src/main/java/org/fifiz/training/java/monolithic_weather_app/WeatherBean.java
+++ src/main/java/org/fifiz/training/java/monolithic_weather_app/WeatherBean.java
@@ -1,18 +1,23 @@
 package org.fifiz.training.java.monolithic_weather_app;
 
-import java.io.Serializable;
-
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+
+import org.fifiz.training.java.monolithic_weather_app.owm.OwmClient;
+import org.fifiz.training.java.monolithic_weather_app.owm.WeatherResult;
 
 @ManagedBean
 @SessionScoped
 public class WeatherBean {
 
     private String codePostal = "79000";
+    private WeatherResult meteo;
 
     public String goToResult() {
-        return "welcome";
+        // appel au service de météo
+        OwmClient owmc = new OwmClient(this.codePostal);
+        WeatherResult weather = OwmClient.getWeather();
+        return weather.getName();
     }
 
     public String getCodePostal() {
--- src/main/java/org/fifiz/training/java/monolithic_weather_app/owm/OwmClient.java
+++ src/main/java/org/fifiz/training/java/monolithic_weather_app/owm/OwmClient.java
@@ -16,35 +16,41 @@ import org.apache.logging.log4j.Logger;
  * @author bertrand
  */
 public class OwmClient {
-    
+
     private static final Logger LOG = LogManager.getLogger(OwmClient.class);
-    
+
     /**
      * URL du serveur
      */
     private URL ownUrl;
-    
+
     private ObjectMapper jsonMapper;
-    
-    public OwmClient(URL ownUrl){
+
+    public OwmClient(URL ownUrl) {
         this.ownUrl = ownUrl;
         this.jsonMapper = new ObjectMapper();
         this.jsonMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
-    
+
+    public OwmClient(String codePostal) {
+        this.ownUrl = "http://api.openweathermap.org/data/2.5/weather?zip={codePostal},fr&APPID=8c05dfed7d5d0d8ba3a2bc70b83b227f";
+        this.jsonMapper = new ObjectMapper();
+        this.jsonMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
     /**
      * 
-     * @return 
+     * @return
      */
-    public WeatherResult getWeather(){
+    public WeatherResult getWeather() {
         try {
             HttpURLConnection ownConnexion = (HttpURLConnection) ownUrl.openConnection();
-            
+
             ownConnexion.connect();
             if (ownConnexion.getResponseCode() != 200) {
-                throw new TechnicalException("Statut invalide "+ownConnexion.getResponseCode());
+                throw new TechnicalException("Statut invalide " + ownConnexion.getResponseCode());
             }
-            
+
             return this.jsonMapper.readValue(ownConnexion.getInputStream(), WeatherResult.class);
         } catch (IOException ex) {
             throw new TechnicalException("Impossible de contacter OWN", ex);
@@ -66,7 +72,5 @@ public class OwmClient {
     public void setJsonMapper(ObjectMapper jsonMapper) {
         this.jsonMapper = jsonMapper;
     }
-    
-    
-    
+
 }
